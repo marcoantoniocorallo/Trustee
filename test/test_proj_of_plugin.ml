159fb643a723be7e6a7ee0be5d73703c1d5e5129
marco <marcoantoniocorallo3@gmail.com>
2024-08-27 15:31:24 +0200

    enhance test

--- test/test_proj_of_plugin.ml
+++ test/test_proj_of_plugin.ml
@@ -11,17 +11,13 @@ let code =
 
 let value = Int(6);;
 
-let%test "proj_plugin" =
-  let (@@) v1 v2 = TFhree.Syntax.compare_values v1 v2 in 
-  let lexbuf = Lexing.from_string code in 
-  let code = TFhree.Parser.main TFhree.Lexer.tokenize lexbuf in 
-  let _ = TFhree.Type_system.type_check code in 
-  let vval = TFhree.Interpreter.eval code in 
-  vval @@ value
-
 let%expect_test "proj_plugin_output" =
   let lexbuf = Lexing.from_string code in 
   let code = TFhree.Parser.main TFhree.Lexer.tokenize lexbuf in 
   let _ = TFhree.Type_system.type_check code in 
-  let _ = TFhree.Interpreter.eval code in 
-  [%expect {| Warning: the computed value can be tainted |}]
+  let res = TFhree.Interpreter.eval code in
+  print_endline (TFhree.Utils.string_of_value res);
+  [%expect {| 
+  Warning: the computed value can be tainted 
+  6
+  |}]
