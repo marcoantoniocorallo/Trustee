79002f147143ab9134a9298d91f98fb1e3a404a4
marco <marcoantoniocorallo3@gmail.com>
2024-09-01 15:59:16 +0200

    update syntax of existing plugins

--- test/manual_test/test_include.ml
+++ test/manual_test/test_include.ml
@@ -1,6 +1,6 @@
 let p =  <"p"> in p
 // let p =  <"test/parsePlugin"> in p // no permissions!
-// let p =  <"plugin"> in p // not a file
+// let p =  <"plugin"> in p // not such file
 // let p =  <"plugin/w"> in p // no such file
 // let p =  <"plugin/../test/parsePlugin"> in p // nope!
 // let p =  <"plugin/ps/p"> in p //nope!
